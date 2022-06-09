058bbffb9118b5548f0d5ae0f5d698370398c675
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-09 19:49:54 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "zeitun",
+    streetAddress: "12th Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, { [key]: value});
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObj = Object.assign({}, employee);
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+
+    return employee;
+}
